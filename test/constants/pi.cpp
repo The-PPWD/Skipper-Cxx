1ee6d6b0e2f35dbd18e98353f291c3603225246d
Patrick Hofmann <patrickhofmanndesigns@gmail.com>
2021-04-19 16:38:02 -0500

    Added proper test

--- test/constants/pi.cpp
+++
@@ -1,7 +0,0 @@
-#include "skipper/constants/pi.hpp"
-
-#include <iostream>
-
-int main(int argc, char* argv[]) {
-    std::cout << skipper::constants::pi_f << std::endl;
-}
---
+++ test/test.cpp
@@ -0,0 +1,5 @@
+#include "skipper/skipper.hpp"
+
+int main(int argc, char* argv[]) {
+    return 0;
+}
